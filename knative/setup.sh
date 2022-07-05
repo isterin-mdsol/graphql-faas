3f40bc3b39bb9f4c4e2d9356ec16366ccb76d23e
isterin-mdsol <isterin@mdsol.com>
2022-07-05 18:01:20 -0400

    Hacking

--- knative/setup.sh
+++ knative/setup.sh
@@ -13,8 +13,24 @@ if (! command -v brew &> /dev/null ); then
   echo "You must have homebrew installed"
   exit 1
 fi
+if (! command -v kind &> /dev/null ); then
+  brew install kind
+fi
+if (! command -v kn &> /dev/null ); then
+  brew install knative/client/kn
+  brew install knative-sandbox/kn-plugins/quickstart
+  kn quickstart kind
+fi
 
-brew install kind
-brew install knative/client/kn
-brew install knative-sandbox/kn-plugins/quickstart
-kn quickstart kind
+for resolver in ./resolvers/{clients,studies,subjects}/service.yaml; do
+    dir=`dirname $resolver`
+    file=`basename $resolver`
+    funcName=`basename $dir`
+    pushd $dir &> /dev/null
+    echo "DIR: ${dir} FILE: ${file} FUNC: $funcName"
+    docker build -t isterin/knative-${funcName}-resolver .
+    docker push isterin/knative-${funcName}-resolver
+    kubectl apply --filename ./${file}
+    kubectl get ksvc knative-${funcName}-resolver --output=custom-columns=NAME:.metadata.name,URL:.status.url
+    popd &> /dev/null
+done
